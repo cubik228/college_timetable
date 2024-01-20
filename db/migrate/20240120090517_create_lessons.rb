1eb347215a2165b7314c9ac48cd2e3112e5b2ff7
cubik228 <mkomp06@gmail.com>
2024-01-20 12:38:14 +0300

    use rubocop

--- db/migrate/20240120090517_create_lessons.rb
+++ db/migrate/20240120090517_create_lessons.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateLessons < ActiveRecord::Migration[7.1]
   def change
     create_table :lessons do |t|
-      t.string :subject 
-      t.integer :teacher_id 
-      t.integer :day_on_wek 
-      
+      t.string :subject
+      t.integer :teacher_id
+      t.integer :day_on_wek
+
       t.timestamps
     end
   end
