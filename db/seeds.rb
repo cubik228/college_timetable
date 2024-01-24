fcdb5a40df9c6eff57182e4318ff5b20f5f6e6e8
herasimchuk2808@gmail.com <you@example.com>
2024-01-24 16:11:08 +0300

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Teacher.destroy_all
+
+5.times do |index|
+  Teacher.create!(name: Faker::Name.name,
+                  room: Faker::House.room)
+end
+puts "Created #{Teacher.count} teachers"
+
+Group.destroy_all
+
+5.times do |index|
+  Group.create!(name: Faker::Name.name,
+                teacher_id: Teacher.pluck(:id).sample)
+end
+puts "Created #{Group.count} groups"
+
+Lesson.destroy_all
+
+5.times do |index|
+  Lesson.create!(subject: Faker::Science.science(:formal, :applied),
+                 teacher_id: Teacher.pluck(:id).sample,
+                 day_on_wek: Faker::Date.forward(days: 23),
+                 group_id: Group.pluck(:id).sample)
+end
+
+puts "Created #{Lesson.count} lessons"
